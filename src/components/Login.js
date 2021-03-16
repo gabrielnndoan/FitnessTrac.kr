456a34d3a1f3cab680b9beb4ccbb1a3afc857ac4
leslieguan <lguan@xula.edu>
2021-03-15 21:24:43 -0500

    Login working without token

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,96 @@
-import { useState } from "react";
+import {React, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { getToken, login } from "../auth";
+
+const Login = ({
+  authenticate,
+  setAuthentication,
+  username,
+  setUsername,
+  setToken
+}) => {
+  const [ password, setPassword ] = useState();
+  const [ loginSuccessful, setLoginSuccessful ] = useState(false);
+  function authentication(event) {
+    event.preventDefault();
+      fetch(
+        'http://fitnesstrac-kr.herokuapp.com/api/users/login',
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // 'Authorization': 'Bearer TOKEN_STRING_HERE'
+          },
+          body: JSON.stringify({
+              username: username,
+              password: password,
+          }),
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          if(!result.success) {
+            alert(result.message)
+          }
+          console.log(result)
+          // login(result.token);
+          // setToken(getToken())
+          // isLoggedIn(result)
+        })
+        .catch(console.error);
+  
+  }
+
+  const isLoggedIn = (result) => {
+    if (result) {
+      console.log("is logged in");
+      setAuthentication(true);
+      setLoginSuccessful(true);
+      alert(result.message)
+    } else {
+      console.log("not logged in")
+      alert(result.message)
+    }
+  }; 
+
+  if (loginSuccessful && authenticate) {
+    return <Redirect to="/myRoutines" />;
+  } 
+
+  return (
+    <div className="registerInput">
+      <h1> Login Page </h1>
+      <form className="form" onSubmit={ authentication }>
+        <label className="userLabel">Username:</label>
+        <input
+          className="userInput"
+          minLength="8"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        ></input>
+
+        <label className="passwordLabel">Password:</label>
+        <input
+          type="password"
+          className="passwordInput"
+          minLength="8"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        ></input>
+        <div className="organizeButtons">
+          <button className="loginButton" type="submit">
+            Login
+          </button>
+          <Link className="registerButton" to="/register">
+            Click to Register
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 
 import { useState } from "react";
 import Register from "./components/Register";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 const App = () => {
@@ -17,7 +18,11 @@ const App = () => {
         <Link to="/routines">ROUTINES</Link>
         <Link to="/myRoutines">MY ROUTINES</Link>
         <Link to="/activities">ACTIVITIES</Link>
-        <Link to="/login">LOGIN</Link>
+        {!authenticate ? (
+              
+                <Link to="/Login">LOGIN</Link>
+      
+            ) : null}
         <Link to="/register">REGISTER</Link>
       </nav>
       <main>
@@ -25,7 +30,15 @@ const App = () => {
           <Route path="/routines"></Route>
           <Route path="/myRoutines"></Route>
           <Route path="/activities"></Route>
-          <Route path="/login"></Route>
+          <Route path="/login">
+          {!authenticate ? (<Login
+              username={username}
+              setUsername={setUsername}
+              setToken={setToken}
+              authenticate={authenticate}
+              setAuthentication={setAuthentication}
+            />) : null }
+          </Route>
           <Route path="/register">
             <Register
               username={username}
